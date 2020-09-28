4c0dde5236bf55f6b4375394bd840b13c5279292
MikeDilworth <mike.dilworth@foxnews.com>
2020-09-28 12:09:13 -0400

    Added support for getting latest early voting report name from AP Atom feed.

--- AP_EarlyVoting_Parser/frmMain.cs
+++ AP_EarlyVoting_Parser/frmMain.cs
@@ -28,8 +28,10 @@ namespace AP_EarlyVoting_Parser
         static DateTime nowTime = DateTime.Now;
         static DateTime scheduledTime;
 
-        // Set data URL
-        public string urlForData = "https://api.ap.org/v2/reports/d34c0da52f2b44389de43a43f05a2289?apikey=xEOmjspBfmqSpyWx5hIPfrDWxsbrKrSv";
+        // Set URLs
+        public string urlForReports = "https://api.ap.org/v2/reports?apikey=xEOmjspBfmqSpyWx5hIPfrDWxsbrKrSv&type=advvotes";
+        public string urlForData = string.Empty; 
+        public string apiKeyString = "?apikey=xEOmjspBfmqSpyWx5hIPfrDWxsbrKrSv";
 
         // Declare background worker thread
         private BackgroundWorker backgroundWorker1;
@@ -49,6 +51,7 @@ namespace AP_EarlyVoting_Parser
 
         // Method to read in the latest state-level early voting data file and post the results to the SQL DB
         private void GetLatestData()
+        //private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
             try
             {
@@ -64,129 +67,176 @@ namespace AP_EarlyVoting_Parser
                 {
                 };
 
-                XmlDocument x = new XmlDocument();
-                x.Load(urlForData);
+                // Get list of reports
+                XmlDocument reports = new XmlDocument();
 
-                XmlNodeList xList = x.SelectNodes("//stateAdvanceTurnout");
+                // Setup namespace for parsing atom feed; force name "Atom" for namespace
+                XmlNamespaceManager nsmgr = new XmlNamespaceManager(reports.NameTable);
+                nsmgr.AddNamespace("atom", "http://www.w3.org/2005/Atom");
 
-                SqlConnection conn = new SqlConnection(config.AppSettings.Settings["sqlConnString"].Value);
-                SqlCommand cmd = new SqlCommand();
+                // Load the Atom feed for the reports
+                reports.Load(urlForReports);
 
-                int rowCount = 0;
+                // Get the list of reports
+                //XmlNodeList reportsList = reports.SelectNodes("//entry");
+                XmlNodeList reportsList = reports.DocumentElement.SelectNodes("atom:entry", nsmgr);
 
+                // Get the report for the advance turnout
                 //Iterates thru each race
-                foreach (XmlNode a in xList)
+                foreach (XmlNode report in reportsList)
                 {
                     try
                     {
-                        stateDataRecord.statePostal = a["statePostal"].InnerText;
+                        // If it's the advance turnout report, build the URL string for the data
+                        if (report["title"].InnerText == "AdvVotes / advturnout2020GE")
+                        {
+                            urlForData = report["id"].InnerText + apiKeyString;
+                        }
                     }
-                    catch (NullReferenceException e)
+                    //catch (Exception e)
+                    catch (Exception)
                     {
-                        stateDataRecord.statePostal = "N/A";
-                    }
-                    try
-                    {
-                        stateDataRecord.mailOrAbsBallotsRequested = a["mailOrAbsBallotsRequested"].InnerText;
-                    }
-                    catch (NullReferenceException e)
-                    {
-                        stateDataRecord.mailOrAbsBallotsRequested = "N/A";
-                    }
-                    try
-                    {
-                        stateDataRecord.mailOrAbsBallotsSent = a["mailOrAbsBallotsSent"].InnerText;
-                    }
-                    catch (NullReferenceException e)
-                    {
-                        stateDataRecord.mailOrAbsBallotsSent = "N/A";
-                    }
-                    try
-                    {
-                        stateDataRecord.mailOrAbsBallotsCast = a["mailOrAbsBallotsCast"].InnerText;
-                    }
-                    catch (NullReferenceException e)
-                    {
-                        stateDataRecord.mailOrAbsBallotsCast = "N/A";
-                    }
-                    try
-                    {
-                        stateDataRecord.earlyInPersonCast = a["earlyInPersonCast"].InnerText;
-                    }
-                    catch (NullReferenceException e)
-                    {
-                        stateDataRecord.earlyInPersonCast = "N/A";
-                    }
-                    try
-                    {
-                        stateDataRecord.totalAdvVotesCast = a["totalAdvVotesCast"].InnerText;
-                    }
-                    catch (NullReferenceException e)
-                    {
-                        stateDataRecord.totalAdvVotesCast = "N/A";
-                    }
-                    try
-                    {
-                        stateDataRecord.dateofLastUpdate = a["dateofLastUpdate"].InnerText;
-                    }
-                    catch (NullReferenceException e)
-                    {
-                        stateDataRecord.dateofLastUpdate = "N/A";
-                    }
-                    stateDataRecord.updateDateTime = DateTime.Now;
-
-                    try
-                    {
-                        rowCount++;
-
-                        conn.Open();
-                        cmd = new SqlCommand(config.AppSettings.Settings["storedProcedure"].Value, conn);
-                        cmd.CommandType = CommandType.StoredProcedure;
-
-                        cmd.Parameters.Add(new SqlParameter("@StateMnemonic", stateDataRecord.statePostal));
-
-                        cmd.Parameters.Add(new SqlParameter("@Year", 2020));
-
-                        int ballotsSentRequested = Extensions.ParseInt(stateDataRecord.mailOrAbsBallotsSent, 0) + Extensions.ParseInt(stateDataRecord.mailOrAbsBallotsRequested, 0);
-                        cmd.Parameters.Add(new SqlParameter("@BallotsSentRequested", ballotsSentRequested));
-
-                        int ballotsReturned = Extensions.ParseInt(stateDataRecord.mailOrAbsBallotsCast, 0);
-                        cmd.Parameters.Add(new SqlParameter("@BallotsReturned", ballotsReturned));
-
-                        int earlyInPersonVote = Extensions.ParseInt(stateDataRecord.earlyInPersonCast, 0);
-                        cmd.Parameters.Add(new SqlParameter("@EarlyInPersonVote", earlyInPersonVote));
-
-                        int totalAdvanceTurnout = Extensions.ParseInt(stateDataRecord.totalAdvVotesCast, 0);
-                        cmd.Parameters.Add(new SqlParameter("@TotalAdvanceTurnout", totalAdvanceTurnout));
-
-                        cmd.Parameters.Add(new SqlParameter("@DateOfLastUpdate", stateDataRecord.dateofLastUpdate));
-
-                        //cmd.Parameters.Add(new SqlParameter("@UpdateDateTime", SqlDbType.DateTime).Value = stateDataRecord.updateDateTime);
-
-                        cmd.ExecuteNonQuery();
-
-                        conn.Close();
-
-                        logTxt.AppendText("Processed data for state: " + stateDataRecord.statePostal + Environment.NewLine);
-                    }
-                    catch (Exception e)
-                    {
-                        logTxt.AppendText("Error occurred during database posting on row " + rowCount.ToString() + ": " + e.Message + Environment.NewLine);
+                        //logTxt.AppendText("Error occurred while trying to retrieve report name:" + e.Message + Environment.NewLine);
+                        logTxt.AppendText("Error occurred while trying to retrieve report name" + Environment.NewLine);
                     }
                 }
-              
-                conn.Close();
-                // Display stats for processing
-                elapsed.Stop();
-                logTxt.AppendText(Environment.NewLine + "Last state data update processed: " + DateTime.Now.ToString() + Environment.NewLine);
-                logTxt.AppendText("Data rows processed: " + rowCount.ToString() + Environment.NewLine);
-                logTxt.AppendText("Total elapsed time (seconds): " + elapsed.Elapsed.TotalSeconds + Environment.NewLine);
-                txtStatus.Text = "State-Level data early voting data update completed at: " + DateTime.Now.ToString();
+
+                if (urlForData != string.Empty)
+                {
+                    // Get data
+                    XmlDocument x = new XmlDocument();
+
+                    x.Load(urlForData);
+
+                    XmlNodeList xList = x.SelectNodes("//stateAdvanceTurnout");
+
+                    SqlConnection conn = new SqlConnection(config.AppSettings.Settings["sqlConnString"].Value);
+                    SqlCommand cmd = new SqlCommand();
+
+                    int rowCount = 0;
+
+                    //Iterates thru each race
+                    foreach (XmlNode a in xList)
+                    {
+                        try
+                        {
+                            stateDataRecord.statePostal = a["statePostal"].InnerText;
+                        }
+                        catch (NullReferenceException)
+                        {
+                            stateDataRecord.statePostal = "N/A";
+                        }
+                        try
+                        {
+                            stateDataRecord.mailOrAbsBallotsRequested = a["mailOrAbsBallotsRequested"].InnerText;
+                        }
+                        catch (NullReferenceException)
+                        {
+                            stateDataRecord.mailOrAbsBallotsRequested = "N/A";
+                        }
+                        try
+                        {
+                            stateDataRecord.mailOrAbsBallotsSent = a["mailOrAbsBallotsSent"].InnerText;
+                        }
+                        catch (NullReferenceException)
+                        {
+                            stateDataRecord.mailOrAbsBallotsSent = "N/A";
+                        }
+                        try
+                        {
+                            stateDataRecord.mailOrAbsBallotsCast = a["mailOrAbsBallotsCast"].InnerText;
+                        }
+                        catch (NullReferenceException)
+                        {
+                            stateDataRecord.mailOrAbsBallotsCast = "N/A";
+                        }
+                        try
+                        {
+                            stateDataRecord.earlyInPersonCast = a["earlyInPersonCast"].InnerText;
+                        }
+                        catch (NullReferenceException)
+                        {
+                            stateDataRecord.earlyInPersonCast = "N/A";
+                        }
+                        try
+                        {
+                            stateDataRecord.totalAdvVotesCast = a["totalAdvVotesCast"].InnerText;
+                        }
+                        catch (NullReferenceException)
+                        {
+                            stateDataRecord.totalAdvVotesCast = "N/A";
+                        }
+                        try
+                        {
+                            stateDataRecord.dateofLastUpdate = a["dateofLastUpdate"].InnerText;
+                        }
+                        catch (NullReferenceException)
+                        {
+                            stateDataRecord.dateofLastUpdate = "N/A";
+                        }
+                        stateDataRecord.updateDateTime = DateTime.Now;
+
+                        try
+                        {
+                            rowCount++;
+
+                            conn.Open();
+                            cmd = new SqlCommand(config.AppSettings.Settings["storedProcedure"].Value, conn);
+                            cmd.CommandType = CommandType.StoredProcedure;
+
+                            cmd.Parameters.Add(new SqlParameter("@StateMnemonic", stateDataRecord.statePostal));
+
+                            cmd.Parameters.Add(new SqlParameter("@Year", 2020));
+
+                            int ballotsSentRequested = Extensions.ParseInt(stateDataRecord.mailOrAbsBallotsSent, 0) + Extensions.ParseInt(stateDataRecord.mailOrAbsBallotsRequested, 0);
+                            cmd.Parameters.Add(new SqlParameter("@BallotsSentRequested", ballotsSentRequested));
+
+                            int ballotsReturned = Extensions.ParseInt(stateDataRecord.mailOrAbsBallotsCast, 0);
+                            cmd.Parameters.Add(new SqlParameter("@BallotsReturned", ballotsReturned));
+
+                            int earlyInPersonVote = Extensions.ParseInt(stateDataRecord.earlyInPersonCast, 0);
+                            cmd.Parameters.Add(new SqlParameter("@EarlyInPersonVote", earlyInPersonVote));
+
+                            int totalAdvanceTurnout = Extensions.ParseInt(stateDataRecord.totalAdvVotesCast, 0);
+                            cmd.Parameters.Add(new SqlParameter("@TotalAdvanceTurnout", totalAdvanceTurnout));
+
+                            cmd.Parameters.Add(new SqlParameter("@DateOfLastUpdate", stateDataRecord.dateofLastUpdate));
+
+                            //cmd.Parameters.Add(new SqlParameter("@UpdateDateTime", SqlDbType.DateTime).Value = stateDataRecord.updateDateTime);
+
+                            cmd.ExecuteNonQuery();
+
+                            conn.Close();
+
+                            logTxt.AppendText("Processed data for state: " + stateDataRecord.statePostal + Environment.NewLine);
+                        }
+                        catch (Exception e)
+                        //catch (Exception)
+                        {
+                            logTxt.AppendText("Error occurred during database posting on row " + rowCount.ToString() + ": " + e.Message + Environment.NewLine);
+                            //logTxt.AppendText("Error occurred during database posting on row " + rowCount.ToString() + Environment.NewLine);
+                        }
+                    }
+
+                    conn.Close();
+                    // Display stats for processing
+                    elapsed.Stop();
+                    logTxt.AppendText(Environment.NewLine + "Last state data update processed: " + DateTime.Now.ToString() + Environment.NewLine);
+                    logTxt.AppendText("Data rows processed: " + rowCount.ToString() + Environment.NewLine);
+                    logTxt.AppendText("Total elapsed time (seconds): " + elapsed.Elapsed.TotalSeconds + Environment.NewLine);
+                    txtStatus.Text = "State-Level data early voting data update completed at: " + DateTime.Now.ToString();
+                }
+                else
+                {
+                    logTxt.AppendText("Could not determine URL for advance voting report" + Environment.NewLine);
+                }
 
             }
             catch (Exception e)
+            //catch (Exception)
             {
                 txtStatus.Text = "Error occurred during state-level data retrieval and database posting: " + e.Message;
+                //txtStatus.Text = "Error occurred during state-level data retrieval and database posting";
             }
         }
 
@@ -194,7 +244,7 @@ namespace AP_EarlyVoting_Parser
         void schedule_Timer()
         {
             //Console.WriteLine("### Timer Started ###");
-           //logTxt.AppendText("### Timer Started ###" + Environment.NewLine);
+           logTxt.AppendText("### Timer Started ###" + Environment.NewLine);
 
            nowTime = DateTime.Now;
            scheduledTime = new DateTime(nowTime.Year, nowTime.Month, nowTime.Day, 06, 0, 0, 0); // Start at 6:00 AM
@@ -216,12 +266,19 @@ namespace AP_EarlyVoting_Parser
         void timer_Elapsed(object sender, ElapsedEventArgs e)
         {
             // Update status label
-            txtStatus.Text = "Update timer fired at " + DateTime.Now.ToString();
+            //txtStatus.Text = "Update timer fired at " + DateTime.Now.ToString();
+            logTxt.AppendText("Update timer fired at " + DateTime.Now.ToString() + Environment.NewLine);
 
             // Call method with flag set to download the latest data; if false, filename is specified as 2nd parameter
             //GetLatestData_County(true, String.Empty);
 
             GetLatestData();
+            // Start worker thread to get latest state-level data
+            //if (backgroundWorker1.IsBusy != true)
+            //{
+            //    backgroundWorker1.RunWorkerAsync();
+            //}
+
 
             // Stop the timer
             timer.Stop();
@@ -232,9 +289,7 @@ namespace AP_EarlyVoting_Parser
 
         private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            // Sleep 2 seconds to emulate getting data.
-            Thread.Sleep(2000);
-            e.Result = "This text was set safely by BackgroundWorker.";
+            GetLatestData();
         }
 
         private void BackgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
@@ -244,6 +299,10 @@ namespace AP_EarlyVoting_Parser
 
         private void btnGetLatestStateData_Click(object sender, EventArgs e)
         {
+            //if (backgroundWorker1.IsBusy != true)
+            //{
+            //    backgroundWorker1.RunWorkerAsync();
+            //}
             GetLatestData();
         }
 
